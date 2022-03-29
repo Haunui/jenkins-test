e06f292a7b0818439452b82c547aa8bc9fbfb6ac
Haunui Saint-sevin <haunui@saint-sevin.fr>
2022-03-29 10:47:52 +0200

    Test

--- main.c
+++ main.c
@@ -1,7 +1,42 @@
 #include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#define PORT 80
 
-int main() {
-	printf("hello world\n");
-	printf("jk build test\n");
-	return 0;
+int main(int argc, char const *argv[]) {
+  if(argc <= 0) {
+    printf("Target website not specified");
+    return -1;
+  }
+
+  int sock = 0, valread;
+  struct sockaddr_in serv_addr;
+  char* str = "HEAD / HTTP/1.1";
+  char buffer[1024] = {0};
+
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("\n Socket creation error \n");
+    return -1;
+  }
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(PORT);
+
+  if(inet_pton(AF_INET, argv[0], &serv_addr.sin_addr) <= 0) {
+    printf("\nInvalid address/ Address not supported \n");
+    return -1;
+  }
+
+  if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    printf("\nConnection Failed \n");
+    return -1;
+  }
+
+  send(sock, str, strlen(str), 0);
+  printf("HTTP req. sent\n");
+  valread = read(sock, buffer, 1024);
+  printf("%s\n", buffer);
+  return 0;
 }
